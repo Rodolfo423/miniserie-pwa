a36e10fe0f96fd57744a7086b85ddd62f7bdbd83
Rodolfo <rcasalfo@gmail.com>
2021-09-16 21:24:17 -0500

    Primer commit

---
+++ script.js
@@ -0,0 +1,5 @@
+if('serviceWorkers' in navigator){
+    navigator.serviceWorkers.register('./sw.js')
+    .then(reg =>console.log('Registro de SW exitoso', reg))
+    .catch(err =>console.warn('Error al tratar de registrar el sw', err))
+}
